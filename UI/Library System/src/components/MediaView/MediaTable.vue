b3c2c823e76a157e8981dbd24b0731c544c7a061
Henry <henry_barker99@hotmail.com>
2024-11-11 15:17:20 +0000

    Get count stuff working

---
+++ UI/Library System/src/components/MediaView/MediaTable.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: "MediaTable",
+  data() {
+    return {
+      mediaData: Media[]
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+      this.message = "Incremented";
+    }
+  }
+})
+
+</script>
+
+<template>
+  <div>
+    {{count}}
+  </div>
+
+  <button @click="increment()">Click me</button>
+
+</template>
+
+<style scoped>
+</style>

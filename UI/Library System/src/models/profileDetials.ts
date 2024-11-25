10a84fe369823ae27c1a97cab5339073353ee588
14ellisj <93329091+14ellisj@users.noreply.github.com>
2024-11-25 14:17:31 +0000

    Models

--- UI/Library System/src/models/profileDetials.ts
+++
@@ -1,8 +0,0 @@
-export interface ProfileDetails {
-    email: string
-    first_name: string
-    last_name: string
-    date_of_birth: string
-    password: string
-  }
-  
--- UI/Library System/src/stores/profileInformation.ts
+++ UI/Library System/src/stores/profileInformation.ts
@@ -1,6 +1,6 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import type { ProfileDetails } from '@/models/profileDetials'
+import type { ProfileDetails } from '@/models/profile'
 
 export const useUserStore = defineStore('user', {
   state: () => ({

beb63742f599254fc0c097ddc40f277592fc68de
Henry <henry_barker99@hotmail.com>
2024-11-25 12:25:35 +0000

    Author FIlter Tests

---
+++ Services/Media Serivce.Tests/Controllers/MediaControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using Media_Service.Database;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media_Serivce.Tests.Controllers
+{
+    [TestClass]
+    public class MediaControllerTests
+    {
+        Mock<AppDbContext> _appDbContextMock;
+        
+
+        public MediaControllerTests()
+        {
+            _appDbContextMock = new Mock<AppDbContext>();
+
+            
+        }
+
+        [TestMethod]
+        public void Get_NoFilters_ShowAllResults()
+        {
+            
+        }
+    }
+}

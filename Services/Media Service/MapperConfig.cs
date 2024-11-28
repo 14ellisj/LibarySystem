83f554d2ebaaa7a594e13bc1db1500de18d15d30
14ellisj <93329091+14ellisj@users.noreply.github.com>
2024-11-28 12:48:54 +0000

    Mapping Fix

--- Services/Media Service/MapperConfig.cs
+++ Services/Media Service/MapperConfig.cs
@@ -8,6 +8,7 @@ namespace Media_Service
         public MapperConfig()
         {
             CreateMap<MediaEntity, Media>()
+                .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => Enum.Parse<MediaType>(src.type.name.ToUpper())))
                 .ForMember(dest => dest.Type, opt => opt.MapFrom(src => Enum.Parse<MediaType>(src.type.name.ToUpper())))
                 .ForMember(dest => dest.IsAvailable, opt => opt.MapFrom(src => src.media_items.Any(mi => mi.borrower == null)));
 
@@ -15,8 +16,6 @@ namespace Media_Service
                 .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.first_name))
                 .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.last_name));
 
-            CreateMap<GenreEntity, Genre>();
-                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.name))
         }
     }
 }

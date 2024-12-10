7d9901dd365779ed74b67470cf7c8f0811e993ed
14ellisj <93329091+14ellisj@users.noreply.github.com>
2024-12-10 13:18:40 +0000

    Remove Media Service

--- Services/Media Service/Models/Specifications/ISpecification.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq.Expressions;
-
-namespace Media_Service.Models.Specifications
-{
-    public interface ISpecification<T>
-    {
-        Expression<Func<T, bool>> Criteria { get; }
-    }
-}
--- Services/Media Service/Services/IAutoCompleteService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Media_Service.Models;
-
-namespace Media_Service.Services
-{
-    public interface IAutoCompleteService
-    {
-        Task<IEnumerable<string>> GetAutoComplete(string query, SearchType searchType);
-    }
-}
